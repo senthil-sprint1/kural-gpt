0f9b0cbef9cbca7b6ddd0c3f696144d5f8be540b
senthil-sprint1 <senthil@Sprint1Software.com>
2023-12-26 10:47:52 -0600

    Added education

--- kural-gpt-search-pg/scripts/main.ts
+++ kural-gpt-search-pg/scripts/main.ts
@@ -2,14 +2,15 @@ import { search } from "./search";
 
 async function main(): Promise<void> {
   const mom = "What makes a mom delightful?";
-  const education = "What does thirukural say about learning?";
+  const education = "What does thirukural say about being a student?";
+  const educationTamil = "கல்வியை பற்றி திருக்குறள் என்ன சொல்கிறது?";
   const betrayal = "What does thirukural say about betrayal?";
   const grateful = "What does thirukural say about thanking?"; // 138
   const determination = "What does thirukural say about determination?"; // 666 determination and focus
   const notKilling = "What does thirukural say about killing?"; // 324 determination and focus
   const falsehood = "What does thirukural say about lying?"; // 292 determination and focus
 
-  const matchingKurals = await search({ query: falsehood });
+  const matchingKurals = await search({ query: educationTamil });
   console.log(`Found ${matchingKurals.length}":`);
   for (const iKural of matchingKurals) {
     var kural = iKural.kural_json;
