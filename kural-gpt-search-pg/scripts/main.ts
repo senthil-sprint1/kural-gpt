d8f07ca0ee75b72a30c5d9233850ecd7c950fb3f
senthil-sprint1 <senthil@Sprint1Software.com>
2023-12-26 10:02:22 -0600

    More questions

--- kural-gpt-search-pg/scripts/main.ts
+++ kural-gpt-search-pg/scripts/main.ts
@@ -1,6 +1,21 @@
-import { populateKuralTable } from "./populateKuralTable";
 import { search } from "./search";
 
-const mom = "What makes a mom delightful?";
-const query = "With joy the hearts of parents swell";
-search({ query: mom });
+async function main(): Promise<void> {
+  const mom = "What makes a mom delightful?";
+  const education = "What does thirukural say about learning?";
+  const betrayal = "What does thirukural say about betrayal?";
+  const grateful = "What does thirukural say about thanking?"; // 138
+  const determination = "What does thirukural say about determination?"; // 666 determination and focus
+  const notKilling = "What does thirukural say about killing?"; // 324 determination and focus
+  const falsehood = "What does thirukural say about lying?"; // 292 determination and focus
+
+  const matchingKurals = await search({ query: falsehood });
+  console.log(`Found ${matchingKurals.length}":`);
+  for (const iKural of matchingKurals) {
+    var kural = iKural.kural_json;
+
+    console.log(`Kural ${kural.Number} \n ${kural.Line1} \n ${kural.Line2} \n ${kural.explanation} \n ${kural.mk} \n`);
+  }
+}
+
+main();
--- kural-gpt-search-pg/scripts/search.ts
+++ kural-gpt-search-pg/scripts/search.ts
@@ -1,13 +1,25 @@
 import { getEmbedding, supabaseClient } from "./helper";
 
-export async function search({ query }: { query: string }): Promise<void> {
+export async function search({ query }: { query: string }): Promise<KuralTable[]> {
   const embedding = await getEmbedding(query);
 
   const { data: kurals } = await supabaseClient.rpc("search_kural", {
     query_embedding: embedding,
     match_threshold: 0.5,
-    max_result_to_return: 10,
+    max_result_to_return: 20,
   });
 
-  console.log("search results", kurals);
+  return kurals;
 }
+
+export type KuralTable = {
+  kural_json: Kural;
+  kural_number: number;
+};
+export type Kural = {
+  Line1: string;
+  Line2: string;
+  explanation: string;
+  mk: string;
+  Number: number;
+};
